df69e15da3e199d988773cd5b5acf93f860e7ef6
Nate <natelostintimeandspace@gmail.com>
2025-04-05 18:05:19 -0400

    Created HAL interface.
    Signed-off-by: Nate <natelostintimeandspace@gmail.com>

--- code.py
+++ code.py
@@ -1,9 +1,7 @@
-import board
 import terminalio
 import displayio
 from adafruit_display_text import label
-from rainbowio import colorwheel
-from hardware import button, display, pixel, board_pixel, encoder, DISPLAY_HEIGHT, DISPLAY_WIDTH, main_splash, tsc
+from hardware import HAL
 from character_sheet import PulpCharacter
 from file_manager import FileManager
 
@@ -11,66 +9,48 @@ FileManager.mount_sdcard()
 ana_path = FileManager.get_character_path("pulp_cthulhu", "ana_engel")
 ana = PulpCharacter(ana_path)
 
-display.rotation = 270
-# Draw a green background
-color_bitmap = displayio.Bitmap(DISPLAY_HEIGHT, DISPLAY_WIDTH, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0x00FF00  # Bright Green
-
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-
-main_splash.append(bg_sprite)
-
-# Draw a smaller inner rectangle
-inner_bitmap = displayio.Bitmap(220, 150, 1)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = 0xAA0088  # Purple
-inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=10, y=10)
-main_splash.append(inner_sprite)
+HAL.show_credits_screen()
+HAL.draw_main_background()
 
 # Draw a label
 text_group = displayio.Group(scale=3, x=40, y=40)
 text = f"{ana.name}"
 text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
 text_group.append(text_area)  # Subgroup for text scaling
-main_splash.append(text_group)
+HAL.main_splash().append(text_group)
 
 # Draw another label
 pos_text_group = displayio.Group(scale=2, x=40, y=80)
 pos_text = "Skill:"
 pos_text_area = label.Label(terminalio.FONT, text=pos_text, color=0xFFFF00)
 pos_text_group.append(pos_text_area)
-main_splash.append(pos_text_group)
-display.root_group = main_splash
+HAL.main_splash().append(pos_text_group)
 
 last_position = -1
 color = 0  # start at red
 
 while True:
     # negate the position to make clockwise rotation positive
-    position = -encoder.position
+    position = -HAL.get_encoder_position()
 
     if position != last_position:
         tmp = position % len(ana.skills)
         pos_text_area.text = f"{ana.skills[tmp]}: {ana.get_value_at(ana.skills[tmp])}"
-        if button.value:
+        if HAL.is_button_pressed():
             # Change the LED color.
             if position > last_position:  # Advance forward through the colorwheel.
                 color += 1
             else:
                 color -= 1  # Advance backward through the colorwheel.
             color = (color + 256) % 256  # wrap around to 0-256
-            pixel.fill(colorwheel(color))
-            board_pixel.fill(colorwheel(color))
+            HAL.fill_all_pixels(color)
 
         else:  # If the button is pressed...
             # ...change the brightness.
-            if position > last_position:  # Increase the brightness.
-                pixel.brightness = min(1.0, pixel.brightness + 0.1)
-                board_pixel.brightness = min(1.0, pixel.brightness + 0.1)
+            if position > last_position:
+                HAL.increase_all_pixel_brightness()
 
-            else:  # Decrease the brightness.
-                pixel.brightness = max(0, pixel.brightness - 0.1)
-                board_pixel.brightness = max(0, pixel.brightness - 0.1)
+            else:
+                HAL.decrease_all_pixel_brightness()
 
     last_position = position
--- hardware.py
+++ hardware.py
@@ -16,24 +16,24 @@ except ImportError:
     from displayio import FourWire
 
 seesaw = seesaw.Seesaw(board.STEMMA_I2C(), addr=0x36)
-
 seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
-print("Found product {}".format(seesaw_product))
-if seesaw_product != 4991:
-    print("Wrong firmware loaded?  Expected 4991")
+
+DEBUG = False
+if DEBUG:
+    print("Found product {}".format(seesaw_product))
+    if seesaw_product != 4991:
+        print("Wrong firmware loaded?  Expected 4991")
 
 # Configure seesaw pin used to read knob button presses
 # The internal pull up is enabled to prevent floating input
 seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
 
-# TODO: Game Interface component
 button = digitalio.DigitalIO(seesaw, 24)
 encoder = rotaryio.IncrementalEncoder(seesaw)
 pixel = neopixel.NeoPixel(seesaw, 6, 1)
 pixel.brightness = 0.5
 
 # Metro's on-board Neopixel
-# TODO: Game Interface component
 board_pixel = np.NeoPixel(board.NEOPIXEL, 1)
 board_pixel.brightness = 0.5
 
@@ -49,53 +49,136 @@ DISPLAY_WIDTH = 320
 DISPLAY_HEIGHT = 240
 
 display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_rst)
-
-# TODO: Game Interface component
 display = adafruit_ili9341.ILI9341(display_bus, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT)
 
 # Make the display context
-main_splash = displayio.Group()
-display.root_group = main_splash
+# TODO: Decide if main_splash should be global
+main_display_group = displayio.Group()
+display.root_group = main_display_group
 
 irq_dio = None
+# TODO: Game interface component
 tsc = adafruit_tsc2007.TSC2007(board.I2C(), irq=irq_dio)
 
 
-# TODO: refactor to use Adafuit Slideshow.
-def show_title_touch_screen():
-    logo = FileManager.get_image_path("LogoLTS")
-    pythulhu = FileManager.get_image_path("pythulhu")
-    splash = displayio.Group()
-    groups = []
-    images = [logo, pythulhu]
+class HAL:
+    """
+    The HAL acts as the primary interface between a game's logic and the hardware.
+    This isn't a HAL in the traditional sense, but serves a similar purpose here.
 
-    for i in range(len(images)):
+    TODO: Decide how games should display stuff.
+    TODO: Add touch screen interface.
+    """
+
+    @classmethod
+    def rotate_display(cls, degrees: int)-> None:
+        display.rotation = degrees
+
+    @classmethod
+    def display_text(cls, group_index: int, text: str) -> None:
+        display.root_group[group_index].text = text
+
+    @classmethod
+    def display_image(cls, img_path: str) -> None:
+        pass
+
+    @classmethod
+    def main_splash(cls):
+        return display.root_group
+
+    @classmethod
+    def is_button_pressed(cls) -> bool:
+        return button.value
+
+    @classmethod
+    def get_encoder_position(cls) -> int:
+        return encoder.position
+
+    @classmethod
+    def fill_pixel(cls, color: int) -> None:
+        pixel.fill(colorwheel(color))
+
+    @classmethod
+    def fill_metro_pixel(cls, color: int) -> None:
+        board_pixel.fill(colorwheel(color))
+
+    @classmethod
+    def fill_all_pixels(cls, color: int) -> None:
+        pixel.fill(colorwheel(color))
+        board_pixel.fill(colorwheel(color))
+
+    @classmethod
+    def increase_all_pixel_brightness(cls) -> None:
+        pixel.brightness = min(1.0, pixel.brightness + 0.1)
+        board_pixel.brightness = min(1.0, pixel.brightness + 0.1)
+
+    @classmethod
+    def decrease_all_pixel_brightness(cls) -> None:
+        pixel.brightness = max(0, pixel.brightness - 0.1)
+        board_pixel.brightness = max(0, pixel.brightness - 0.1)
+
+    @classmethod
+    def draw_main_background(cls):
+        """
+        Draws the default background to the display.
+        NOTE: The display defaults to potrait mode layout.
+
+        TODO: allow for customization
+        """
+        display.root_group = main_display_group
+
+        # Draw a green background
+        color_bitmap = displayio.Bitmap(DISPLAY_HEIGHT, DISPLAY_WIDTH, 1)
+        color_palette = displayio.Palette(1)
+        color_palette[0] = 0x00FF00  # Bright Green
+
+        bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+
+        main_display_group.append(bg_sprite)
+
+        # Draw a smaller inner rectangle
+        inner_bitmap = displayio.Bitmap(220, 150, 1)
+        inner_palette = displayio.Palette(1)
+        inner_palette[0] = 0xAA0088  # Purple
+        inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=10, y=10)
+        main_display_group.append(inner_sprite)
+        display.rotation = 270
+
+    @classmethod
+    def show_credits_screen(cls):
+        # TODO: refactor to use Adafuit Slideshow.
+        logo = FileManager.get_image_path("LogoLTS")
+        pythulhu = FileManager.get_image_path("pythulhu")
         splash = displayio.Group()
-        bitmap = displayio.OnDiskBitmap(images[i])
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
-        splash.append(tile_grid)
-        groups.append(splash)
+        groups = []
+        images = [logo, pythulhu]
 
-    index = 0
-    touch_state = False
-    display.root_group = groups[index]
-    while True:
-        if tsc.touched and not touch_state:
-            point = tsc.touch
-            touch_state = True
-            if point["pressure"] < 200:  # ignore touches with no 'pressure' as false
-                continue
+        for i in range(len(images)):
+            splash = displayio.Group()
+            bitmap = displayio.OnDiskBitmap(images[i])
+            tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
+            splash.append(tile_grid)
+            groups.append(splash)
 
-            # left side of the screen
-            if point["y"] < 2000:
-                index = (index - 1) % len(images)
-            # right side of the screen
-            else:
-                index = (index + 1) % len(images)
-                if (index == 0):
-                    displayio.release_displays()
-                    break
+        index = 0
+        touch_state = False
+        display.root_group = groups[index]
+        while True:
+            if tsc.touched and not touch_state:
+                point = tsc.touch
+                touch_state = True
+                if point["pressure"] < 200:  # ignore touches with no 'pressure' as false
+                    continue
 
-            display.root_group = groups[index]
-        if not tsc.touched and touch_state:
-            touch_state = False
+                # left side of the screen
+                if point["y"] < 2000:
+                    index = (index - 1) % len(images)
+                # right side of the screen
+                else:
+                    index = (index + 1) % len(images)
+                    if (index == 0):
+                        break
+
+                display.root_group = groups[index]
+            if not tsc.touched and touch_state:
+                touch_state = False
