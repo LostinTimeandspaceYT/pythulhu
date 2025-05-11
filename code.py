9c743053788817de828d3ea9d705e1f03ba21d90
Nate <natelostintimeandspace@gmail.com>
2025-05-11 13:22:34 -0400

    Navigation now functional.
    Signed-off-by: Nate <natelostintimeandspace@gmail.com>

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ from hardware import HAL
 from touch_ui import TouchManager, LightTouchButton
 from coc_character import PulpCharacter
 from file_manager import FileManager
-from menu import Menu
+from menu_panel import MenuPanel
 from skills_panel import SkillsPanel
 from ui_context import UIContext
 
@@ -11,7 +11,6 @@ from ui_context import UIContext
 FileManager.mount_sdcard()
 HAL.init()
 manager = TouchManager(HAL.tsc)
-HAL.display.root_group = HAL.root_group
 
 ana_path = FileManager.get_character_path("pulp_cthulhu", "ana_engel")
 ana = PulpCharacter(ana_path)
@@ -22,29 +21,12 @@ nav_buttons = {
     "back": LightTouchButton("back", 110, 200, 100, 30, "Back"),
 }
 
-# Showing the items on the screen
-menus = {}
-
 context = UIContext(
     hal=HAL,
     manager=manager,
     character=ana,
     nav_buttons=nav_buttons
 )
-
-def activate_nav_buttons(*names):
-    for name, btn in nav_buttons.items():
-        if name in names:
-            btn.attach_to(HAL.root_group)
-            btn.register(manager)
-        else:
-            btn.remove_from(HAL.root_group)
-            btn.unregister(manager)
-
-def go_to_menu(menu_name):
-    def inner_callback(button):
-        menus[menu_name].show()
-    return inner_callback
 
 def show_skills_menu(button):
     HAL.clear_display()
@@ -59,21 +41,15 @@ def show_skills_menu(button):
 
     context.switch_to("skills")
 
-    nav_buttons["prev"].callback = lambda b: panel.prev_page()
-    nav_buttons["next"].callback = lambda b: panel.next_page()
-    nav_buttons["back"].callback = lambda b: context.transition_back()
-    activate_nav_buttons("prev", "next", "back")
-
 # Define menus
 main_buttons = [
     LightTouchButton("skills", 50, 80, 220, 30, "Skills", callback=show_skills_menu),
     LightTouchButton("quit", 50, 130, 220, 30, "Quit", callback=lambda b: print("Quit"))
 ]
 
-menus["main"] = Menu("Main", main_buttons, hal=HAL, manager=manager)
-
-# Show initial screen
-menus["main"].show()
+main_menu = MenuPanel(context, main_buttons)
+context.register_panel("main", main_menu)
+context.transition_to("main")
 
 while True:
     manager.poll()
