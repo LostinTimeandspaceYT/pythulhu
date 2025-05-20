b0858c0819bb8ea8d9514403a66e878e3340d6cc
Nathan <natelostintimeandspace@gmail.com>
2025-05-19 22:33:26 -0400

    Created function to limit scope of game factory.
    Signed-off-by Nathan <natelostintimeandspace@gmail.com>

--- code.py
+++ code.py
@@ -10,26 +10,36 @@ from ui_context import UIContext
 FileManager.mount_sdcard()
 HAL.init()
 manager = TouchManager(HAL.tsc)
-
-
 nav_buttons = {
     "prev": LightTouchButton("prev", 10, 200, 80, 30, "Prev"),
     "next": LightTouchButton("next", 230, 200, 80, 30, "Next"),
     "back": LightTouchButton("back", 110, 200, 100, 30, "Back"),
 }
-
 context = UIContext(
     hal=HAL,
     manager=manager,
     nav_buttons=nav_buttons
 )
+gc.collect()
 
-def start_game(runner: GameRunner):
-    runner.start()
+def start_main_menu():
+    from game_factory import GameFactory
+    from game_runner import GameRunner
 
-factory = GameFactory(ui_context=context)
-factory.select_and_build_runner(on_runner_ready=start_game)
+    def start_game(runner: GameRunner):
+        print("[DEBUG] Game setup complete.")
+        runner.start()
 
+    print("[DEBUG] Starting game factory setup...")
+    factory = GameFactory(ui_context=context)
+    factory.select_and_build_runner(on_runner_ready=start_game)
+
+
+gc.collect()
+print("[DEBUG] Before game factory, mem_free:", gc.mem_free())
+
+# Kick off game AFTER initial boot
+start_main_menu()
 
 while not context.should_exit:
     manager.poll()
